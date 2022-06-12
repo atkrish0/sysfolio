13ce37fee2979d9485e522652f69a52feb79fbbd
Atheesh Krishnan <atheesh.krishnan@outlook.com>
2022-06-12 16:21:32 +0530

    reformat 2

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup
 
 setup(
-    name = 'initial',
-    version = '0.0.1',
-    description = 'treading water',
-    py_modules =  ["init"],
-    package_dir = {'': 'src'}
+    name='initial',
+    version='0.0.1',
+    description='treading water',
+    py_modules=["init"],
+    package_dir={'': 'src'}
 )
