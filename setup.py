31b774c8b1fc4d008c7f53596601deea7caa3154
Atheesh Krishnan <atheesh.krishnan@outlook.com>
2022-06-12 16:07:58 +0530

    setup details

--- setup.py
+++ setup.py
@@ -1,1 +1,9 @@
-# standby
+from setuptools import setup
+
+setup(
+    name = 'initial',
+    version = '0.0.1',
+    description = 'treading water',
+    py_modules =  ["init"],
+    package_dir = {'': 'src'}
+)
